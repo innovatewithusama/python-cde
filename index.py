c52813fc2958566d88a0f2c5a8535554062a178e
musama4421 <musama4421@gmail.com>
2025-03-09 11:01:49 +0500

    new files

---
+++ index.py
@@ -0,0 +1,14 @@
+print("Esc + R convert cell to RAW - Plain text and Esc + Y will convert cell back to code")
+print("This is my VS code python")
+print("This is raw")
+
+usama_bio = [
+    "Name: Muhammad Usama",
+    "F.Name: Muhammad Sabir",
+    "Occupation: Student",
+    "Course: Cloud Data Engineering",
+    "Center: Saylani Mass IT Program",
+    "Roll Number: 456987",
+    "I am a student odf saylani mass it training program. I am currently learning CDE at saylani. My instruction name is Qasim Hasan"
+]
+print(usama_bio)
